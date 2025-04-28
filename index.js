9bad7856654c2a96c592cfc976c6d2cc39826b80
Emilyempa <emilypettersson@hotmail.com>
2025-04-28 16:10:12 +0200

    Add BigInt prototype toJSON method for JSON serialization

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import router from './routes/user.js';
 
-
 const PORT = process.env.PORT || 3000;
 
 const app=express();
 
+
+BigInt.prototype.toJSON = function() { return this.toString(); };
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
