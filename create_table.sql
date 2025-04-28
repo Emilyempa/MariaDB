1aace1484774f16ac85c38191cf5c51bc7f7d1cf
Emilyempa <emilypettersson@hotmail.com>
2025-04-28 15:10:15 +0200

    Add SQL script to create user table with necessary fields

---
+++ create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE testing.`user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+)
+ ENGINE=InnoDB 
+ DEFAULT CHARSET=utf8mb4 
+ COLLATE=utf8mb4_uca1400_ai_ci
