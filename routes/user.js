b6990fea9c4770d0ac65c1e950f60b74cb7010c3
Emilyempa <emilypettersson@hotmail.com>
2025-04-29 11:05:30 +0200

    Refactor user registration to hash passwords before storing and implement login functionality with password validation

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pool from "../helpers/database.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -16,9 +17,11 @@ router.get("/:id", async function (req, res) {
 router.post("/register", async function (req, res) {
   try {
     const { email, password } = req.body;
-    console.log("Received data:", { email, password }); 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const sqlQuery = `INSERT INTO user (email, password) VALUES (?, ?)`;
-    const result = await pool.query(sqlQuery, [email, password]);
+    const result = await pool.query(sqlQuery, [email, hashedPassword]);
+
     res.status(201).json({userId: result.insertId});
   } catch (error) {
     console.error("Database error:", error); 
@@ -27,6 +30,29 @@ router.post("/register", async function (req, res) {
     } else {
       res.status(500).json({ error: error.message }); 
     }
+  }
+});
+
+router.post("/login", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    const sqlQuery = `SELECT id, password FROM user WHERE email = ?`;
+    const rows = await pool.query(sqlQuery, email);
+
+    if (rows.length === 0) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const user = rows[0];
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    res.status(200).json({ valid_password: isPasswordValid});
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
